990c0d044d3b481292b870603ee822ddeeaa764a
James Landry <james.j.landry@outlook.com>
2020-05-22 00:43:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  charlie = Float::INFINITY
+  name_hash.each do |kye, val|
+    if val < charlie
+      charlie = val
+    end
+  end
+  frank = nil
+  name_hash.each do |kye, val|
+    if charlie == val
+      frank = kye
+    end
+  end
+  frank
 end
